66089522995ebf66f60897f5067f3427a8df123d
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-09-13 18:16:59 +0100

    Added loader component animation

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -1,16 +1,32 @@
-'use client'
-import { useEffect } from 'react'
-import { motion } from 'framer-motion'
+"use client";
+import { useEffect } from "react";
+import { motion } from "framer-motion";
 
-const container = {
-  hidden: {
-    opacity: 0
+const loader = {
+  initial: {
+    opacity: 0,
+    translateY: "0",
+  },
+  animate: {
+    opacity: 1,
+    translateY: "-100vh",
+  },
+  exit: {
+    opacity: 0,
+    translateY: "-100vh",
   }
-}
+};
 
 const Loader = () => {
   return (
-    <motion.div className="w-screen h-screen flex justify-center items-center bg-white absolute top-0 left-0 translate-y-[0%] transition-all duration-700">
+    <motion.div
+      variants={loader}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 0.75 }}
+      className="w-screen h-screen flex justify-center items-center bg-white absolute top-0 left-0 translate-y-[0%] transition-all duration-700"
+    >
       <h1 className="text-wrap text-[10vw] font-semibold">
         ADV/MNSTRY<span className="text-[3vw]">®</span>
       </h1>
