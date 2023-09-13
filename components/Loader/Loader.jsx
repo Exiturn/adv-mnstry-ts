e7565dffc7fbccef2e769515b24cb4ac346f6783
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-09-13 16:50:46 +0100

    Added Loader component and styles

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Loader = () => {
   return (
-    <div className='w-screen h-screen bg-green-400 absolute top-0 left-0'>Loader</div>
-  )
-}
+    <div className="w-screen h-screen flex justify-center items-center bg-white absolute top-0 left-0">
+      <h1 className="text-wrap text-[10vw] font-semibold">
+        ADV/MNSTRY<span className="text-[3vw]">®</span>
+      </h1>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
