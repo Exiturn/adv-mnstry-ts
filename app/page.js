92d0d4f805de810f911605c5d222c054d2884461
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-09-13 17:51:39 +0100

    Added Lenis smooth scrolling

--- app/page.js
+++ app/page.js
@@ -1,8 +1,26 @@
+"use client";
 import Hero from "@/components/Hero/Hero";
 import Awards from "@/components/Awards/Awards";
 import Loader from "@/components/Loader/Loader";
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  useEffect( () => {
+    const lenis = new Lenis();
+
+    lenis.on('scroll', (e) => {
+      console.log(e)
+    })
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div className="relative">
       <Hero />
--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+'use client'
+import { useEffect } from 'react'
+import { motion } from 'framer-motion'
+
+const container = {
+  hidden: {
+    opacity: 0
+  }
+}
 
 const Loader = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-white absolute top-0 left-0">
+    <motion.div className="w-screen h-screen flex justify-center items-center bg-white absolute top-0 left-0 translate-y-[0%] transition-all duration-700">
       <h1 className="text-wrap text-[10vw] font-semibold">
         ADV/MNSTRY<span className="text-[3vw]">®</span>
       </h1>
-    </div>
+    </motion.div>
   );
 };
 
