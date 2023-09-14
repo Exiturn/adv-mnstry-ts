4cda21564b77369fdf417ba10d0e1c3a50bca123
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-09-14 13:59:12 +0100

    Overflow-x hidden added to html

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div className="w-screen h-screen relative">
-      <video className="absolute w-screen h-screen top-0 left-0 z-[-1] object-cover pointer-events-none" autoPlay muted loop>
+      <video className="absolute w-screen h-screen top-0 left-0 z-[-1] object-cover pointer-events-none" autoPlay muted loop playsInline>
         <source src={"/video.mp4"} type="video/mp4"/>
       </video>
     </div>
