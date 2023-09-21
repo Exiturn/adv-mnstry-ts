fca810e431328be3dac0904acad1bd768549138d
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-09-21 13:42:14 +0100

    Prettified Hero component

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,17 +1,29 @@
-'use client'
-import { useRef } from 'react'
-import { useCursorTracker } from '@/hooks/useCursorTracker';
+"use client";
+import { useRef } from "react";
+import { useCursorTracker } from "@/hooks/useCursorTracker";
 
 const Hero = () => {
   const targetRef = useRef(null);
   const cursorPosition = useCursorTracker(targetRef);
   return (
-    <div className="w-screen h-screen relative flex justify-center items-center overflow-y-hidden" ref={targetRef}>
-      <video className="absolute w-screen h-screen top-0 left-0 z-[-1] object-cover pointer-events-none" autoPlay muted loop playsInline>
-        <source src={"/video.mp4"} type="video/mp4"/>
+    <div
+      className="w-screen h-screen relative flex justify-center items-center overflow-y-hidden"
+      ref={targetRef}
+    >
+      <video
+        className="absolute w-screen h-screen top-0 left-0 z-[-1] object-cover pointer-events-none"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src={"/video.mp4"} type="video/mp4" />
       </video>
-      <div className="relative flex justify-center items-center bg-[#f4f4f4] rounded-full w-[7rem] h-[7rem] p-4 uppercase text-center font-bold leading-tight duration-75 ease-linear heroCta" 
-      style={{ transform: `translate(${cursorPosition.x}px, ${cursorPosition.y}px)` }}
+      <div
+        className="relative flex justify-center items-center bg-[#f4f4f4] rounded-full w-[7rem] h-[7rem] p-4 uppercase text-center font-bold leading-tight duration-75 ease-linear heroCta"
+        style={{
+          transform: `translate(${cursorPosition.x}px, ${cursorPosition.y}px)`,
+        }}
       >
         <p>Watch Reel</p>
       </div>
