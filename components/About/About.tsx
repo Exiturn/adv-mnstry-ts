27ec4f425035f0feb56d84ef2857fb3eac2a506a
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-09-22 18:54:38 +0100

    Added top section of About component

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,7 +1,24 @@
 const About: React.FC = () => {
   return (
-    <div className='w-screen h-[80vh]'>About</div>
-  )
-}
+    <main className="w-screen h-[80vh] pb-[15vw] px-[5.4vw] xl:px-[8rem]">
+      <figure className="h-[0.1rem] w-full bg-black mb-[8.5em]" />
+      <section className="flex flex-col xl:flex-row items-start xl:items-center gap-5">
+        <div className="w-full md:w-[50vw] flex flex-col justify-evenly items-start">
+          <span className="text-[1.5em] md:text-[3em] leading-[1.2] font-medium">
+            ADV/MNSTRY® is a global branding and digital design agency building
+            products, services, and eCommerce experiences that turn cultural
+            values into company value.
+          </span>
+          <button className="w-[50vw] md:w-[12vw] rounded-full border-[1px] py-[0.5rem] px-[2.5vw] mt-[4em] border-black text-center text-[1em] font-bold uppercase">
+            See the work
+          </button>
+        </div>
+        <div className="w-auto place-self-end xl:place-self-center">
+          <h1 className="text-[20vw] xl:text-[14vw] font-extrabold">A/M®</h1>
+        </div>
+      </section>
+    </main>
+  );
+};
 
-export default About
+export default About;
