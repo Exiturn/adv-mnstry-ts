ad81a60feabc8e9e1aaad59210fbb56a2022ea8a
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-10-04 17:50:50 +0100

    Added engagement component / Interface for engagement object array / styling and layout for draggable horizontal list

--- components/Engagements/Engagements.tsx
+++ components/Engagements/Engagements.tsx
@@ -1,94 +1,115 @@
 import { useCursorTracker } from "@/hooks/useCursorTracker";
 import Image from "next/image";
+import { useRef } from "react";
+import { useDraggable } from "react-use-draggable-scroll";
 
 interface IEngagementItem {
-    title: string;
-    src: string;
-    alt: string;
-    paragraph: string;
+  title: string;
+  src: string;
+  alt: string;
+  paragraph: string;
 }
 
 const engagementItems: IEngagementItem[] = [
-    {
-        title: 'Google',
-        src: '/assets/brands/google.png',
-        alt: 'Google logo',
-        paragraph: `Our partnership with Google is as deep as it gets. We're the lead
+  {
+    title: "Google",
+    src: "/assets/brands/google.png",
+    alt: "Google logo",
+    paragraph: `Our partnership with Google is as deep as it gets. We're the lead
         creative agency for Google Store and provide strategy, design, and
-        prototyping to other divisions. Learn more about our partnership`
-    },
-    {
-        title: 'KFC',
-        src: '/assets/brands/kfc.png',
-        alt: 'KFC Logo',
-        paragraph: `An award-winning global, digital transformation engagement spanning eCommerce, mobile app, and new drive thru experiences. Bringing KFC's brand story to life while making it easier for customers to buy chicken. Learn more about our partnership`
-    }
-]
+        prototyping to other divisions. Learn more about our partnership `,
+  },
+  {
+    title: "KFC",
+    src: "/assets/brands/kfc.png",
+    alt: "KFC Logo",
+    paragraph: `An award-winning global, digital transformation engagement spanning eCommerce, mobile app, and new drive thru experiences. Bringing KFC's brand story to life while making it easier for customers to buy chicken. Learn more about our partnership `,
+  },
+  {
+    title: "Manors Golf",
+    src: "/assets/brands/manorsgolflogo.webp",
+    alt: "Manors Golf Logo",
+    paragraph: `A reimagining of Manors Golf's brand visual identity, and brand campaign, to support a new product drop and the launch of the first brick and mortar retail locatio in the brand's recent history. Read our full case study `,
+  },
+  {
+    title: "AT&T",
+    src: "/assets/brands/attlogo.webp",
+    alt: "AT&T Logo",
+    paragraph: `Redesigning the digital flagship for the largest telecommunications company in the world. Creating frictionless paths to purchase for a wide range of consumers across a vast portfolio of products and services. Read our full case study `,
+  },
+  {
+    title: `Arc'teryx`,
+    src: "/assets/brands/arclogo.png",
+    alt: `Arc'teryx logo`,
+    paragraph: `Ongoing partnership providing strategy, branding, experience design, and development focused on bringing their mission and offerings to consumers through brand-led programs and platforms. Read our full case study `,
+  },
+];
 
 const Engagement: React.FC = () => {
+  const ref =
+    useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+  const { events } = useDraggable(ref);
+
   return (
-    <li className="w-[30%] flex flex-col justify-start">
-      <section className="mb-[6.5em]">
-        <Image
-          className="mb-[1em]"
-          src="/assets/brands/google.png"
-          width={115}
-          height={115}
-          alt="ad age logo"
-        />
-        <figure className="bg-black h-[0.2em] w-[2em] ml-2" />
-      </section>
-    </li>
-  );
-};
-
-const Engagements: React.FC = () => {
-  return (
-    <main className="w-screen pb-[10vw] px-[5.4vw] xl:px-[8rem]">
-      <section className="mb-[6em]">
-        <figure className="h-[0.1em] w-full bg-black" />
-        <div className="row flex justify-between items-center font-medium w-full">
-          <div className="col">00</div>
-          <div className="col flex justify-between items-center w-[60%]">
-            <span>/05</span>
-            <span className="text-[2em]">●</span>
-          </div>
-        </div>
-      </section>
-
-      <section className="mb-[10em]">
-        <h1 className="uppercase text-[3.5em] leading-[0.95] font-semibold mt-[2rem]">
-          Featured <br />
-          Engagements
-        </h1>
-      </section>
-
-      <ul className="min-w-[100%] h-[40vh] flex overflow-x-auto scrollbar-hide">
-        <li className="w-[30%] flex flex-col justify-start">
+    <div
+      className="h-[60vh] w-full flex overflow-x-auto scrollbar-hide space-x-[15em]"
+      ref={ref}
+    >
+      {engagementItems.map((item) => (
+        <li className="min-w-[25vw] flex flex-col justify-start" key={item.title}>
           <section className="mb-[6.5em]">
-            <Image
-              className="mb-[1em]"
-              src="/assets/brands/google.png"
-              width={115}
-              height={115}
-              alt="ad age logo"
-            />
-            <figure className="bg-black h-[0.2em] w-[2em] ml-2" />
+            <div>
+              <Image
+                className={`mb-[1em] ${item.title === 'AT&T' ? 'w-[4em] h-[4em]' : 'w-[10em] h-[4em]'}`}
+                src={item.src}
+                width={100}
+                height={100}
+                alt={item.alt}
+              />
+            </div>
+            <figure className="bg-black h-[0.1em] w-[2em] ml-2" />
           </section>
-
-          <h2 className="text-[1.5em] uppercase font-bold mb-[1em]">Google</h2>
+          <h2 className="text-[1.5em] uppercase font-bold mb-[1em]">
+            {item.title}
+          </h2>
           <p className="text-[1.25em] font-medium">
-            Our partnership with Google is as deep as it gets. We're the lead
-            creative agency for Google Store and provide strategy, design, and
-            prototyping to other divisions. Learn more about our partnership{" "}
+            {item.paragraph}{" "}
             <a href="/" className="underline">
               here
             </a>
             .
           </p>
         </li>
-      </ul>
-    </main>
+      ))}
+    </div>
+  );
+};
+
+const Engagements: React.FC = () => {
+  return (
+    <>
+      <main className="w-screen pb-[10vw] px-[5.4vw] xl:px-[8rem]">
+        <section className="mb-[6em]">
+          <figure className="h-[0.1em] w-full bg-black" />
+          <div className="row flex justify-between items-center font-medium w-full">
+            <div className="col">00</div>
+            <div className="col flex justify-between items-center w-[60%]">
+              <span>/05</span>
+              <span className="text-[2em]">●</span>
+            </div>
+          </div>
+        </section>
+
+        <section className="mb-[10em]">
+          <h1 className="uppercase text-[3.5em] leading-[0.95] font-semibold mt-[2rem]">
+            Featured <br />
+            Engagements
+          </h1>
+        </section> 
+        <Engagement />
+      </main>
+     
+    </>
   );
 };
 
